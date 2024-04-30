3e0a96c96c1e3823867e65ab84ff5cfcf836f395
João Pedro <jopedroribeiro9@gmail.com>
2024-04-30 13:43:58 -0300

    deploy

--- src/lib/mongodb.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-export async function connectMongoDB() {
-  try {
-    await mongoose.connect(process.env.MONGODB_URL);
-  } catch (error) {
-    console.log("Erro:", error);
-  }
-};
